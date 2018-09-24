c9b1bbcd60cd0f69babaafa72bc6657a9ecd0ee1
Alexander Kras'ko <0m3r.mail@gmail.com>
2018-09-24 18:16:31 +0300

    Rename to sql2mage_installer.php

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ php tool for generating  magento ddl installer script
 
 #usage
 ~~~bash
-$ php -f sql2mage_ddl_installer.php 127.0.0.1 user password magento tm_helpmate_status
+$ php -f sql2mage_installer.php 127.0.0.1 user password magento tm_helpmate_status
 
 *********************************************************************************
 CREATE TABLE `tm_helpmate_status` (
@@ -14,27 +14,48 @@ CREATE TABLE `tm_helpmate_status` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COMMENT='Statuses Table'
 *********************************************************************************
-$table = $installer->getConnection()
-    ->newTable($installer->getTable('tm_helpmate_status'))
-    ->addColumn('id', Varien_Db_Ddl_Table::TYPE_INTEGER, 10, array(
-        'identity'  => true,
-        'unsigned'  => true,
-        'nullable'  => false,
-        'primary'   => true,
-    ), 'Id')
-    ->addColumn('name', Varien_Db_Ddl_Table::TYPE_VARCHAR, 50, array(
-        'nullable'  => true,
-        'default'  => NULL,
-    ), 'Name')
-    ->addColumn('status', Varien_Db_Ddl_Table::TYPE_SMALLINT, 6, array(
-        'default'  => 0,
-    ), 'Status')
-    ->addColumn('sort_order', Varien_Db_Ddl_Table::TYPE_SMALLINT, 5, array(
-        'unsigned'  => true,
-        'nullable'  => false,
-        'default'  => 0,
-    ), 'Sort Order')
-;
+/* Tm/Helpmate/Setup/InstallSchema.php */
+<?php namespace Tm\Helpmate\Setup;
+
+use Magento\Framework\Setup\InstallSchemaInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+use Magento\Framework\DB\Adapter\AdapterInterface;
+use Magento\Framework\DB\Ddl\Table;
+
+/**
+ * @codeCoverageIgnore
+ */
+class InstallSchema implements InstallSchemaInterface
+{
+    public function install(SchemaSetupInterface , ModuleContextInterface )
+    {
+        $table = $installer->getConnection()
+        ->newTable($installer->getTable('tm_helpmate_status'))
+        ->addColumn('id', \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER, 10, [
+            'identity'  => true,
+            'unsigned'  => true,
+            'nullable'  => false,
+            'primary'   => true,
+        ], 'Id')
+        ->addColumn('name', \Magento\Framework\DB\Ddl\Table::TYPE_TEXT, 50, [
+            'nullable'  => true,
+            'default'  => NULL,
+        ], 'Name')
+        ->addColumn('status', \Magento\Framework\DB\Ddl\Table::TYPE_SMALLINT, 6, [
+            'default'  => 0,
+        ], 'Status')
+        ->addColumn('sort_order', \Magento\Framework\DB\Ddl\Table::TYPE_SMALLINT, 5, [
+            'unsigned'  => true,
+            'nullable'  => false,
+            'default'  => 0,
+        ], 'Sort Order')
+        ;
+        $installer->getConnection()->createTable($table);
+    }
+}
+
+
 *********************************************************************************
 ~~~
 
--- src/Setup/InstallSchemaGenerator.php
+++ src/Setup/InstallSchemaGenerator.php
@@ -31,22 +31,36 @@ class InstallSchemaGenerator extends \Swissup\GeneratorAbstract
         $t = '    ';
         $filename = $this->getFilename();
 
-        $str = "\n/* {$filename} */\n\$table = \$installer->getConnection()\n"
-            . "{$t}->newTable(\$installer->getTable('{$tableName}'))\n";
+        $str = "\n/* {$filename} */\n<?php namespace {$vendor}\\{$moduleName}\\Setup;
+
+use Magento\Framework\Setup\InstallSchemaInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+use Magento\Framework\DB\Adapter\AdapterInterface;
+use Magento\Framework\DB\Ddl\Table;
+
+/**
+ * @codeCoverageIgnore
+ */
+class InstallSchema implements InstallSchemaInterface
+{
+    public function install(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {\n{$t}{$t}\$table = \$installer->getConnection()\n"
+            . "{$t}{$t}->newTable(\$installer->getTable('{$tableName}'))\n";
 
         $arrayStart = $this->magentoVersion == 2 ? '[' : 'array(';
         $arrayEnd = $this->magentoVersion == 2 ? ']' : ')';
 
         foreach ($this->getColumns() as $column) {
-            $str .= "{$t}->addColumn('{$column['name']}', {$column['type']}, {$column['length']}, {$arrayStart}\n" .
-                    ($column['identity'] ? "{$t}{$t}'identity'  => true,\n" : '') .
-                    ($column['unsigned'] ? "{$t}{$t}'unsigned'  => true,\n" : '') .
+            $str .= "{$t}{$t}->addColumn('{$column['name']}', {$column['type']}, {$column['length']}, {$arrayStart}\n" .
+                    ($column['identity'] ? "{$t}{$t}{$t}'identity'  => true,\n" : '') .
+                    ($column['unsigned'] ? "{$t}{$t}{$t}'unsigned'  => true,\n" : '') .
                     ($column['nullable'] !== null ?
-                        "{$t}{$t}'nullable'  => " . ($column['nullable'] ? 'true' : 'false') . ",\n" : '') .
-                    ($column['default'] !== false ? "{$t}{$t}'default'  => {$column['default']},\n" : '') .
+                        "{$t}{$t}{$t}'nullable'  => " . ($column['nullable'] ? 'true' : 'false') . ",\n" : '') .
+                    ($column['default'] !== false ? "{$t}{$t}{$t}'default'  => {$column['default']},\n" : '') .
                     // "'nullable'  => false,\n" .
-                    ($column['primary'] ? "{$t}{$t}'primary'   => true,\n" : '') .
-                    "{$t}{$arrayEnd}, '{$column['comment']}')\n"
+                    ($column['primary'] ? "{$t}{$t}{$t}'primary'   => true,\n" : '') .
+                    "{$t}{$t}{$arrayEnd}, '{$column['comment']}')\n"
             ;
         }
         foreach ($this->getIndexes() as $index) {
@@ -56,8 +70,8 @@ class InstallSchemaGenerator extends \Swissup\GeneratorAbstract
             }
             $fields = implode(", ", $fields);
 
-            $str .= "{$t}->addIndex(\$installer->getIdxName('{$index['table']}', {$arrayStart}{$fields}{$arrayEnd}),\n"
-                . "{$t}{$t}{$arrayStart}{$fields}{$arrayEnd})\n";
+            $str .= "{$t}{$t}->addIndex(\$installer->getIdxName('{$index['table']}', {$arrayStart}{$fields}{$arrayEnd}),\n"
+                . "{$t}{$t}{$t}{$arrayStart}{$fields}{$arrayEnd})\n";
         }
 
         foreach ($this->getForeignKeys() as $key) {
@@ -70,13 +84,15 @@ class InstallSchemaGenerator extends \Swissup\GeneratorAbstract
                 $onUpdate = ', ' . $key['on_update'];
             }
 
-            $str .= "{$t}->addForeignKey("
+            $str .= "{$t}{$t}->addForeignKey("
                 . "\$installer->getFkName('{$tableName}', '{$priColumnName}', '{$refTableName}', '{$refColumnName}'),\n"
-                . "{$t}{$t}'{$priColumnName}', \$installer->getTable('{$refTableName}'), '{$refColumnName}',\n"
-                . "{$t}{$onDelete}{$onUpdate})\n";
+                . "{$t}{$t}{$t}'{$priColumnName}', \$installer->getTable('{$refTableName}'), '{$refColumnName}',\n"
+                . "{$t}{$t}{$onDelete}{$onUpdate})\n";
         }
-        $str .= ";\n\$installer->getConnection()->createTable(\$table);";
-
+        $str .= "{$t}{$t};\n{$t}{$t}\$installer->getConnection()->createTable(\$table);
+    }
+}
+";
         return $str;
     }
 }
